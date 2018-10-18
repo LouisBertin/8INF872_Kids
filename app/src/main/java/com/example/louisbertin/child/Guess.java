39e1c26ec0ba7aaf6a115c1fdd19b2b02bd7b830
LouisBertin <bertin.louis7@gmail.com>
2018-10-18 16:50:23 -0400

    :smile: refactoring all app

---
+++ app/src/main/java/com/example/louisbertin/child/Guess.java
@@ -0,0 +1,97 @@
+package com.example.louisbertin.child;
+
+import android.app.Activity;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.louisbertin.child.helpers.StringHelper;
+
+public class Guess {
+
+    protected TextView drawMe, scoreTextView, timer, drawValue;
+    protected Button go, replay;
+    protected String randomLetter, randomLetter1;
+    protected int score;
+    CountDownTimer countDownTimer;
+    protected Activity currentActivity;
+
+    public Guess(MainActivity activity) {
+        // init text to draw
+        drawMe = (TextView) activity.findViewById(R.id.drawMe);
+        scoreTextView = (TextView) activity.findViewById(R.id.scoreValue);
+        timer = (TextView) activity.findViewById(R.id.timer);
+        drawValue = (EditText) activity.findViewById(R.id.draw);
+
+        go = (Button) activity.findViewById(R.id.button);
+        replay = (Button) activity.findViewById(R.id.replay);
+
+        currentActivity = activity;
+
+        initDrawMe();
+        timer();
+    }
+
+    /**
+     * initialisation
+     */
+    private void initDrawMe() {
+        randomLetter = StringHelper.generateRandomCharacter();
+
+        drawMe.setText(randomLetter);
+        drawMe.setTextColor(StringHelper.generateRandomColor());
+        drawMe.animate().alpha(1).rotation(360).setDuration(2000);
+    }
+
+    /**
+     * manage timer
+     */
+    public void timer() {
+        this.countDownTimer = new CountDownTimer(10 * 1000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timer.setText("" + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                drawValue.setAlpha(0);
+                go.setAlpha(0);
+                replay.setAlpha(1);
+                Toast.makeText(currentActivity, "Dommage! :( ton score est : " + score, Toast.LENGTH_SHORT).show();
+            }
+
+        }.start();
+    }
+
+    protected void drawValue() {
+        String value = drawValue.getText().toString();
+
+        if (value.equals(randomLetter.toLowerCase())) {
+
+            score++;
+            String scoreString = Integer.toString(score);
+            scoreTextView.setText(scoreString);
+
+            randomLetter = StringHelper.generateRandomCharacter();
+            drawMe.setTextColor(StringHelper.generateRandomColor());
+
+            if (score > 4) {
+                randomLetter1 = StringHelper.generateRandomCharacter();
+
+                drawMe.setText(randomLetter + randomLetter1);
+            } else {
+                drawMe.setText(randomLetter);
+            }
+
+            drawMe.clearAnimation();
+            drawMe.animate().rotation(-360).setDuration(2000);
+            Toast.makeText(currentActivity, "Excellent!", Toast.LENGTH_SHORT).show();
+            drawValue.setText(null);
+            this.countDownTimer.start();
+        } else {
+            Toast.makeText(currentActivity, "Essaye encore!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/com/example/louisbertin/child/MainActivity.java
+++ app/src/main/java/com/example/louisbertin/child/MainActivity.java
@@ -1,117 +1,54 @@
 package com.example.louisbertin.child;
 
 import android.content.Intent;
-import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.louisbertin.child.helpers.StringHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    protected TextView drawMe, scoreTextView, timer, drawValue;
-    protected Button go, replay;
-    protected String randomLetter, randomLetter1;
-    protected int score;
-    CountDownTimer countDownTimer;
+    protected Guess guess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // init text to draw
-        drawMe = (TextView) findViewById(R.id.drawMe);
-        scoreTextView = (TextView) findViewById(R.id.scoreValue);
-        timer = (TextView) findViewById(R.id.timer);
-        drawValue = (EditText) findViewById(R.id.draw);
-
-        go = (Button) findViewById(R.id.button);
-        replay = (Button) findViewById(R.id.replay);
-
-        initDrawMe();
-        timer();
-    }
-
-    /**
-     * initialisation
-     */
-    protected void initDrawMe() {
-        randomLetter = StringHelper.generateRandomCharacter();
-
-        drawMe.setText(randomLetter);
-        drawMe.setTextColor(StringHelper.generateRandomColor());
-        drawMe.animate().alpha(1).rotation(360).setDuration(2000);
+         guess = new Guess(MainActivity.this);
     }
 
     public void drawValue(View view) {
-        String value = drawValue.getText().toString();
-
-        if (value.equals(randomLetter.toLowerCase())) {
-
-            score++;
-            String scoreString = Integer.toString(score);
-            scoreTextView.setText(scoreString);
-
-            randomLetter = StringHelper.generateRandomCharacter();
-            drawMe.setTextColor(StringHelper.generateRandomColor());
-
-            if (score > 4) {
-                randomLetter1 = StringHelper.generateRandomCharacter();
-
-                drawMe.setText(randomLetter + randomLetter1);
-            } else {
-                drawMe.setText(randomLetter);
-            }
-
-            drawMe.clearAnimation();
-            drawMe.animate().rotation(-360).setDuration(2000);
-            Toast.makeText(this, "Excellent!", Toast.LENGTH_SHORT).show();
-            this.countDownTimer.start();
-
-        } else {
-            Toast.makeText(this, "Essaye encore!", Toast.LENGTH_SHORT).show();
-
-        }
-
+        guess.drawValue();
     }
 
-    public void timer() {
-         this.countDownTimer = new CountDownTimer(10 * 1000, 1000) {
-
-            public void onTick(long millisUntilFinished) {
-                timer.setText("" + millisUntilFinished / 1000);
-            }
-
-            public void onFinish() {
-                drawValue.setAlpha(0);
-                go.setAlpha(0);
-                replay.setAlpha(1);
-                Toast.makeText(MainActivity.this, "Dommage! :( ton score est : " + score, Toast.LENGTH_SHORT).show();
-            }
-
-        }.start();
-    }
-
+    /**
+     * relaunch intent
+     * @param view
+     */
     public void replay (View view) {
         Intent intent = getIntent();
         finish();
         startActivity(intent);
     }
 
+    /**
+     * manage option menu
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
+    /**
+     * manage option menu items
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
